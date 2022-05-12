4805516a96712e36d8aa6bdf2b43f6aa7730cdf9
AlpKubat <95579755+AlpKubat@users.noreply.github.com>
2022-05-12 23:20:47 +0300

    Add files via upload
    output example:
    
    Dices: 
    2  5
    Dices: 
    1  5
    Dices: 
    2  2
    Dices: 
    3  4
    Dices: 
    5  4
    Dices: 
    4  4
    Dices: 
    2  5
    Dices: 
    6  2
    Dices: 
    5  6
    Dices: 
    2  5

---
+++ RollingTwoDice.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class RollingTwoDice {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0;i<10;i++)
+			rollTwoDice();
+	}
+	
+	public static void rollTwoDice()
+	{
+		Random random = new Random();    // Creating random object from Random class
+		
+		int dice1 = random.nextInt(6) + 1;	// Rolling first dice
+		int dice2 = random.nextInt(6) + 1;  // Rolling second dice
+		
+		System.out.println("Dices: \n"+ dice1 + "  " + dice2);  // dices oto-casting to string
+	}
+
+}
