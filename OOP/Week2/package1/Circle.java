1ace670d1124c0daa82dd677d3cf96ea12d09de4
AlpKubat <95579755+AlpKubat@users.noreply.github.com>
2022-05-18 19:21:21 +0300

    Update Circle.java

--- OOP/Week2/package1/Circle.java
+++ OOP/Week2/package1/Circle.java
@@ -3,15 +3,15 @@ package package1;
 public class Circle {
 
 	public double radius;
-	final double pi = 3.14;
+	final double pi = 3.14;		// defining variables
 	
 	
-	public double calculate_area()
+	public double calculate_area()		
 	{
-		return (pi * radius * radius);
+		return (pi * radius * radius);		// circle area formula is A = π r²
 	}
 	
-	public double calculate_perimeter()
+	public double calculate_perimeter()		// circle perimeter formula is 2πr
 	{
 		return (2 * pi * radius);
 	}
