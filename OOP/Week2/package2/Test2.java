ae3b711a7a563033560b3639d720c8b369ac68a1
AlpKubat <95579755+AlpKubat@users.noreply.github.com>
2022-05-18 19:23:52 +0300

    Update Test2.java

--- OOP/Week2/package2/Test2.java
+++ OOP/Week2/package2/Test2.java
@@ -1,15 +1,15 @@
 package package2;
-import package1.*;
+import package1.*;					// importing package1
 
 public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Circle obj1 = new Circle();
-		obj1.radius = 10.0;
+		obj1.radius = 10.0;			// creating and defining an object
 		
 		obj1.calculate_area();
-		obj1.calculate_perimeter();
+		obj1.calculate_perimeter();		// calling the functions of object
 	}
 
 }
