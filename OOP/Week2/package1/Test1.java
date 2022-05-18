d028c43bb0a5d891b498085f78e0c47c7efa0f6b
AlpKubat <95579755+AlpKubat@users.noreply.github.com>
2022-05-18 19:22:31 +0300

    Update Test1.java

--- OOP/Week2/package1/Test1.java
+++ OOP/Week2/package1/Test1.java
@@ -5,10 +5,10 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		Circle obj1 = new Circle();
-		obj1.radius = 10.0;
+		obj1.radius = 10.0;		// creating and defining a object
 		
 		obj1.calculate_area();
-		obj1.calculate_perimeter();
+		obj1.calculate_perimeter();	// calling the functions of object
 	}
 
 }
